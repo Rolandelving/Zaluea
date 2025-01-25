4168a6ac65b686f529f95fc50d55866c8cfcadd7
Rolandelving <114941207+Rolandelving@users.noreply.github.com>
2025-01-24 23:22:01 -0800

    Update index.js

--- Site/index.js
+++ Site/index.js
@@ -1,25 +1,12 @@
-const form = document.querySelector('form');
-const input = document.querySelector('input');
-
-form.addEventListener('submit', async event => {
-    event.preventDefault();
-    window.navigator.serviceWorker.register('./sw.js', {
-        scope: __uv$config.prefix
-    }).then(() => {
-        let url = input.value.trim();
-        if (!isUrl(url)) url = 'https://www.google.com/search?q=' + url;
-        else if (!(url.startsWith('https://') || url.startsWith('http://'))) url = 'http://' + url;
-
-
-        window.location.href = __uv$config.prefix + __uv$config.encodeUrl(url);
-    });
+document.getElementById('proxy-form').addEventListener('submit', function(e) {
+    e.preventDefault();
+    const url = document.getElementById('url-input').value;
+    const iframe = document.getElementById('proxy-frame');
+    
+    // Adding 'https://' if the user doesn't include it
+    if (!url.startsWith('http://') && !url.startsWith('https://')) {
+        url = 'https://' + url;
+    }
+    
+    iframe.src = url;
 });
-
-function isUrl(val = ''){
-    if (/^http(s?):\/\//.test(val) || val.includes('.') && val.substr(0, 1) !== ' ') return true;
-    return false;
-};
-
-function quickLink(url1) {
-    window.location.href = __uv$config.prefix + url1;
-}
